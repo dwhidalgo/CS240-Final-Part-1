63b9da4816504de2338e2b5015d8151eb3973d69
dwhidalgo <dwhidalgo@gmail.com>
2017-12-05 13:25:27 -0800

    first commit

---
+++ InNOut.java
@@ -0,0 +1,21 @@
+/*
+	Final Part 1
+	
+	Simulates a day at In and Out
+	
+	@author Drue Hidalgo
+	@version 1.0
+*/
+
+import java.util.Random;
+import java.util.Iterator;
+
+public class InNOut
+{
+	public static void main(String[] args) throws EmptyQueueException
+	{
+		Random random = new Random();
+		
+		
+	}
+}
